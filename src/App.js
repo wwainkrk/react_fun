bb9f8adb3e0a965eeee240dc460ffeae351b8ec7
Sebastian Warszawa <warszawa@fev.com>
2020-01-28 16:31:42 +0100

    Export logic with list to another object - function NumberList

--- src/App.js
+++ src/App.js
@@ -22,16 +22,23 @@ const list = [
 ];
 */
 
+function NumberList(props) {
+  const numbers = props.numbers;
+  const doubleNumbers = numbers.map((number) => number*2);
+  const listDobule = doubleNumbers.map((double) =>
+    <li>{double}</li>
+  );
+  return (
+    <ul>{listDobule}</ul>
+  );
+}
+
 class App extends Component {
   render() {
     const numbers = [1,2,3,4,5];
-    const doubleNumbers = numbers.map((number) => number*2)
-    const listDobule = doubleNumbers.map((double) =>
-      <li>{double}</li>
-    )
     return (
       <div className="App">
-        <ul>{listDobule}</ul>
+        <NumberList numbers={numbers}></NumberList>
         {/* <ul>
           {doubleNumbers.map((item) =>
             //console.log(item);
