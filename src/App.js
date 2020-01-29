cadd0fbb16003a09abde7a403ee4b1f0580432c8
Sebastian Warszawa <warszawa@fev.com>
2020-01-29 16:31:15 +0100

    date on screen, 3 level components with properties

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
 import React, { Component } from 'react';
 import './App.css';
 
-function ListItem(props) {
+// function ListItem(props) {
+//   return (
+//     <li>{props.value}</li>
+//   )
+// }
+
+// function NumberList(props) {
+//   const numbers = props.numbers;
+//   return(
+//     <ul>
+//       {numbers.map((number) =>
+//         <ListItem key={number.toString()} value={number*2} />
+//       )}
+//     </ul>
+//   )
+// }
+function Hello (props) {
   return (
-    <li>{props.value}</li>
-  )
+    <h1>Welcome {props.user.name} {props.user.surname}</h1>
+  );
 }
 
-function NumberList(props) {
-  const numbers = props.numbers;
-  return(
-    <ul>
-      {numbers.map((number) =>
-        <ListItem key={number.toString()} value={number*2} />
-      )}
-    </ul>
-  )
+function Clock(props) {
+  return (
+    <div>
+      <Hello user={props.user}/>
+      <h2>Aktualny czas: {props.date.toLocaleTimeString()}</h2>
+    </div>
+  );
 }
 
 class App extends Component {
   render() {
-    const numbers = [1,2,3,4,5];
+    //const numbers = [1,2,3,4,5];
+    const author = {
+        name: 'Sebastian',
+        surname: 'Warszawa'
+    };
+
     return (
       <div className="App">
-        <NumberList numbers={numbers}></NumberList>
+        {/* <NumberList numbers={numbers}></NumberList> */}
+        <Clock user={author} date={new Date()} />
       </div>
-    )
+    );
   }
 }
 
